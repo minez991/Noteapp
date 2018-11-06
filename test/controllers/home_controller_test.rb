1a563730106686b18f6547f3bfd910790c92898d
peter <wf00093@surrey.ac.uk>
2018-11-06 15:37:58 +0000

    added contact page and form

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,10 +4,18 @@ class HomeControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    
+
     assert_select 'title','My Notes'
     assert_select 'h1','My Notes'
     assert_select 'p','Welcome to my notes site!'
   end
-
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_template layout: 'application'
+    assert_select 'title', 'My Notes'
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Complete the following form to get in touch with us.'
+  end
 end
