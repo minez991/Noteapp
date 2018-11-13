756ae23de982420f24503075fd520ed94aee7083
peter <wf00093@surrey.ac.uk>
2018-11-13 14:49:56 +0000

    Added welcome seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+welcome = Note.where(title: 'Welcome').first
+welcome.delete if welcome
+
+notes = Note.create([title: 'Welcome', description: 'To the notes app.'])
