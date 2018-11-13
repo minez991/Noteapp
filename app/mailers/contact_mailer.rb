531629ab0c896044b09601d0b0222e9c0420ee86
peter <wf00093@surrey.ac.uk>
2018-11-13 16:57:58 +0000

    pls end me

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,5 @@
 class ContactMailer < ApplicationMailer
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
->>>>>>> 2b35a1cbf7d9542b037d4e7ffca63df01f8fc00f
-=======
->>>>>>> 2b35a1cbf7d9542b037d4e7ffca63df01f8fc00f
   def contact_email(email, name, telephone, message)
     @email = email
     @name = name
@@ -14,6 +7,4 @@ class ContactMailer < ApplicationMailer
     @message = message
     mail cc: @email
   end
-=======
->>>>>>> 229d2612a65174b6daa3e06681c4b54660d33b21
 end
